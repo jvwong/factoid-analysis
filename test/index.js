904e964f5f166c65411b7750dde18dbf451df65b
Jeffrey Wong <jvwong@gmail.com>
2025-01-14 10:08:12 -0500

    Add test harness.

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Example', function () {
+  it('should put one and one together', function () {
+    expect(1 + 1).to.equal(2);
+  });
+});
